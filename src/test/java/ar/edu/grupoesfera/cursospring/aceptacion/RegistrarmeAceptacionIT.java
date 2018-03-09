4cccdb32305c50b12f2467d49e8cb760dedec9f4
Sebastian Ismael <sebastianismael@gmail.com>
2018-03-09 09:10:12 -0300

    Agrega test de aceptacion para el caso que el usuario ya exista

--- src/test/java/ar/edu/grupoesfera/cursospring/aceptacion/RegistrarmeAceptacionIT.java
+++ src/test/java/ar/edu/grupoesfera/cursospring/aceptacion/RegistrarmeAceptacionIT.java
@@ -4,36 +4,43 @@ package ar.edu.grupoesfera.cursospring.aceptacion;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RegistrarmeAceptacionIT extends TestDeAceptacion {
 
-    private String obtenerContenidoRespuesta(String url) throws Exception {
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.connect();
-        return new BufferedReader(
-                new InputStreamReader(
-                        connection.getInputStream())).readLine();
-    }
-
     @Test
-    public void registroExitoso() {
+    public void siElResgistroEsExitosoDeberiaLlevarAlLogin() {
 
+        // preparacion
         String url = urlBase + "/nuevo-usuario";
-        seleniumDriver.get(url);
-        //System.out.println(seleniumDriver.getPageSource());
 
+        // ejecucion
+        seleniumDriver.get(url);
         seleniumDriver.findElement(By.id("email")).sendKeys("seba@seba.com");
         seleniumDriver.findElement(By.id("password")).sendKeys("1234");
         seleniumDriver.findElement(By.id("btn-registrarme")).click();
 
-        //System.out.println(seleniumDriver.getPageSource());
+        // validacion
         assertThat(seleniumDriver.getPageSource()).contains("validar-login");
+    }
+
+    @Test
+    public void siElUsuarioYaExisteDeberiaVolverARegistrarmeYMostrarError() {
+        // preparacion
+        String url = urlBase + "/nuevo-usuario";
+        seleniumDriver.get(url);
+        seleniumDriver.findElement(By.id("email")).sendKeys("juan@juan.com");
+        seleniumDriver.findElement(By.id("password")).sendKeys("1234");
+        seleniumDriver.findElement(By.id("btn-registrarme")).click();
+
+        // ejecucion
+        seleniumDriver.get(url);
+        seleniumDriver.findElement(By.id("email")).sendKeys("juan@juan.com");
+        seleniumDriver.findElement(By.id("password")).sendKeys("1234");
+        seleniumDriver.findElement(By.id("btn-registrarme")).click();
+
+        // validacion
+        assertThat(seleniumDriver.getPageSource()).contains("El usuario ya existe");
     }
 
 }
